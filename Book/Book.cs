2cdd156933ecf1ac01f53d7f10ae3d7758beacff
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-18 22:16:12 +0300

    Add files via upload

---
+++ Book/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Book : IComparable<Book>
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Book() { }
+        public Book(string name, double price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+        public int CompareTo(Book obj)
+        {
+            return Name.CompareTo(obj.Name);
+        }
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> books = new List<Book>();
+            books.Add(new Book { Name = "Алые паруса", Price = 210 });
+            books.Add(new Book { Name = "Преступление и наказание", Price = 350 });
+            books.Add(new Book { Name = "Доктор Живаго", Price = 135 });
+            books.Add(new Book { Name = "Отцы и дети", Price = 300 });
+            books.Add(new Book { Name = "Война и мир", Price = 400 });
+            books.Sort();
+            foreach (Book elem in books)
+            {
+                Console.WriteLine($"{elem.Name} {elem.Price}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
