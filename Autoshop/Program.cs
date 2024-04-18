61bd2e965626ae56c47c2b245f34a380a4638cd5
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-04-18 22:44:48 +0300

    Add files via upload

--- Autoshop/Program.cs
+++ Autoshop/Program.cs
@@ -12,8 +12,21 @@ namespace Autoshop
         {
             List<Autoshop> autoshops = new List<Autoshop>();
             
-                autoshops.Add(new Autoshop($"Идентификатор: {"ММ 0000 55"}\nМарка: {"УАЗ"}\nМаксимальная скорость: {120}\nСкидка: {30}"));
-        
+            autoshops.Add(new Autoshop { ID = "ММ 0000 55",Stamp = "УАЗ", Speed = 120, Price = 100000, Discount = 30});
+            autoshops.Add(new Autoshop { ID = "ММ 0000 44",Stamp = "Toyota", Speed = 140, Price = 480000, Discount = 10});
+            autoshops.Add(new Autoshop { ID = "ММ 0000 33",Stamp = "Tesla", Speed = 180, Price = 350000, Discount = 15});
+            Console.WriteLine("Все автомобили: ");
+            foreach(Autoshop elem in autoshops)
+            {
+                Console.WriteLine($"ID: {elem.ID}\tStamp: {elem.Stamp}\tSpeed: {elem.Speed}\tPrice: {elem.Price}\tDiscount: {elem.Discount}");
+            }
+            autoshops.Sort();
+            Console.WriteLine("\nАвтомобили, отсортированные по цене: ");
+            foreach(var elem in autoshops)
+            {
+                Console.WriteLine($"ID: {elem.ID}\tStamp: {elem.Stamp}\tSpeed: {elem.Speed}\tPrice: {elem.Price}\tDiscount: {elem.Discount}");
+            }
+            Console.ReadKey();
         }
     }
 }
